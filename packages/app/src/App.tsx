233f6f266368ea99533424943218a625bb9da0d4
kimyooeco <58503130+kimyoo04@users.noreply.github.com>
2023-10-07 01:17:29 +0900

    initialize monorepo project

---
+++ packages/app/src/App.tsx
@@ -0,0 +1,26 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Button } from "@yoojacha/ui";
+
+function App() {
+  const a = 1;
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ shared/ui/components/Button.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function Counting() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>더하기</button>
+    </div>
+  );
+}
