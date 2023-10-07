c181715726066c3bb63f087903e2f5b325744a20
kimyooeco <58503130+kimyoo04@users.noreply.github.com>
2023-10-07 11:50:31 +0900

    eslint, tsconfig 설정 변경

--- configs/eslint-ts/index.js
+++ configs/eslint-ts/index.js
@@ -1,38 +1,19 @@
 /** @type {import('eslint').Linter.BaseConfig} **/
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['react', '@typescript-eslint', 'react-hooks', 'import', '@typescript-eslint/eslint-plugin'],
-    rules: {
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': 'warn',
-        'linebreak-style': 0,
-        quotes: ['error', 'single'],
-        semi: ['error', 'never'],
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': ['error'],
-        'react/jsx-filename-extension': ['warn', { extensions: ['.tsx', '.ts'] }],
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
-        'import/prefer-default-export': 'off',
-        'react/prop-types': 'off',
-        'import/newline-after-import': ['warn', { count: 1 }],
-        'react/react-in-jsx-scope': 'off',
-    },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-}
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
